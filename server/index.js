b23851c7bfc2cce2ce8a009afb306eb4d4fbdf93
Christian Bueno <christianjongbueno@gmail.com>
2020-02-07 11:58:47 -0800

    changed static route to include id

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const path = require('path');
 const port = 3000;
 const app = express();
 
-app.use('/:sku', express.static(path.join(__dirname, '../client/dist')));
+app.use('/:id', express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(port, () => console.log(`Main App listening on port ${port}`));
